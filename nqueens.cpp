44b8850e91edc6741875460c6420efc395555353
Anthony R. Lupica <arl127@uakron.edu>
2022-09-04 23:54:24 -0400

    Create nqueens.cpp

---
+++ nqueens.cpp
@@ -0,0 +1,8 @@
+/* 
+    nqueens.cpp
+
+    author: Anthony Lupica <ar127@uakron.edu>
+    purpose: solve the NQueens problem using the backtracking algorithm for some .csv input 
+             representing a chessboard of size N x N. If a solution is found for N queens, 
+             a .csv solution is produced. Otherwise, no solution is displayed.
+*/
