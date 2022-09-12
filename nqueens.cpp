48d47c6df222c5a91649e83fdb2285b0cb330e84
Anthony R. Lupica <arl127@uakron.edu>
2022-09-12 17:08:01 -0400

    Fix typo 2

--- nqueens.cpp
+++ nqueens.cpp
@@ -15,7 +15,7 @@
                  * Does not validate against the occurance of non-square input,
                    or input that does not adhere to strictly comma-separated 0's and 1's.
                    If these inputs are made, infinite recursion may arise.
-                 * 1 initial queen (1) must be present in some starting position as a given condition.
+                 * One initial queen (1) must be present in some starting position as a given condition.
                    Future versions may add functionality for the option to omit an initial queen.
     
     Implementation:
